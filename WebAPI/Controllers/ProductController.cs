716185e67c6bde7d0b8d452c41655da3cce10c6f
xuanhoanhvn21 <126251835+SONBACHTANG@users.noreply.github.com>
2024-05-29 18:24:32 +0700

    add

---
+++ WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        // GET: api/<ProductController>
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            List<Product> products = new List<Product>();
+            products.Add(new Product()
+            {
+                Id = 1,
+                Name = "Rock",
+                Quantity = 1
+            });
+            products.Add(new Product()
+            {
+                Id = 2,
+                Name = "Paper",
+                Quantity = 3
+            });
+            products.Add(new Product()
+            {
+                Id = 3,
+                Name = "Scissors",
+                Quantity = 5
+            });
+            products.Add(new Product()
+            {
+                Id = 4,
+                Name = "Well",
+                Quantity = 2500
+            });
+            return products;
+        }
+
+        // GET api/<ProductController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ProductController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ProductController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
